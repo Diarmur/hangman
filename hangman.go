5c964b40b26da9712f9cc5bbc5a48614c9ea3f32
smathis <mathis.silotia@ynov.com>
2022-11-16 13:44:17 +0100

    fix

--- hangman.go
+++ hangman.go
@@ -56,11 +56,11 @@ func IsPresent(wordToFind string, choosenLetter string) bool { // func returne t
 }
 
 func OnlyLetter(choosenLetter string) bool {
-	err := true
+	err := false
 	
 	letter := strings.Replace(choosenLetter, "\n", "", -1)
 	if letter < "a" || letter > "z" {
-		err = false
+		err = true
 	}
 
 	return err
